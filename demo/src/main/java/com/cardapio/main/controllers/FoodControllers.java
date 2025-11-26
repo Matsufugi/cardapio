b2ffa05fcb30aae82dc7c730dde76a884f4a7fb9
Matheus Alves <matt.my.23@gmail.com>
2025-11-26 10:12:19 -0300

    :sparkles: Added RegisterFood

--- demo/src/main/java/com/cardapio/main/controllers/FoodControllers.java
+++ demo/src/main/java/com/cardapio/main/controllers/FoodControllers.java
@@ -2,11 +2,11 @@ package com.cardapio.main.controllers;
 
 import com.cardapio.main.modules.Food;
 import com.cardapio.main.services.FoodServices;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +21,12 @@ public class FoodControllers {
     public ResponseEntity<List<Food>> getAll() {
         List<Food> allFoods = service.getAll();
         return ResponseEntity.ok(allFoods);
+    }
+
+    @PostMapping
+    public ResponseEntity<Food> registerFood(@RequestBody @Valid Food food) {
+        Food registerFood = service.register(food);
+        return ResponseEntity.status(HttpStatus.CREATED).body(registerFood);
     }
 
 }
--- demo/src/main/java/com/cardapio/main/services/FoodServices.java
+++ demo/src/main/java/com/cardapio/main/services/FoodServices.java
@@ -2,6 +2,7 @@ package com.cardapio.main.services;
 
 import com.cardapio.main.modules.Food;
 import com.cardapio.main.repository.IFoodRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,10 @@ public class FoodServices {
 
     public List<Food> getAll() {
         return repository.findAll();
+    }
+
+    public Food register(@Valid Food food) {
+        return repository.save(food);
+
     }
 }
